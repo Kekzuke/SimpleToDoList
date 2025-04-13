67f570ad79250c4771beb3c5dc1f8eeede1e8a79
Kekzuke <artem.golub2019@gmail.com>
2025-04-14 02:05:09 +0300

    Initial commit

---
+++ src/main/java/com/Kekzuke/app/SimpleToDoList/SimpleToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.Kekzuke.app.SimpleToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kekzuke/app/SimpleToDoList/SimpleToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kekzuke.app.SimpleToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
